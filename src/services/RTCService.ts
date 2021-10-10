592ca655fe8f00411ea05f39627ed3b1c6ede34e
aleksandr safronov <VTB4054673@region.vtb.ru>
2021-10-10 16:13:06 +0500

    rtc

--- src/services/RTCService.ts
+++ src/services/RTCService.ts
@@ -1,5 +1,30 @@
 
+const serversConfig: RTCConfiguration = {
+    iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
+}
 
 export class RTCService {
 
+    static setUpPeers(localStream: MediaStream) {
+        const localPeerConnection = new RTCPeerConnection(serversConfig)
+        localPeerConnection.addEventListener('addstream', handleAddStream)
+        localPeerConnection.addEventListener('icecandidate', handleConnection)
+        localPeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange)
+
+        localStream.getTracks().forEach(t => localPeerConnection.addTrack(t, localStream))
+
+        localPeerConnection.createOffer()
+
+        function handleAddStream() {
+            console.log(333);
+        }
+
+        function handleConnection() {
+            console.log(111);
+        }
+
+        function handleConnectionChange() {
+            console.log(222);
+        }
+    }
 }
